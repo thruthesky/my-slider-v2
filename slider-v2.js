259afdbfb344f00b1c47d909647f3d4f248b194f
thruthesky <thruthesky@gmail.com>
2016-03-10 21:26:06 +0900

    work for updating banner

--- slider-v2.js
+++ slider-v2.js
@@ -6,7 +6,8 @@ $(function() {
         direction : 'right-to-left',
         active : 0,
         zIndex : 100,
-        mouseIn : false
+        mouseIn : false,
+        dotColor : '#494949'
     };
     var el = {
         slider : function() {
@@ -16,7 +17,6 @@ $(function() {
             return el.slider().find('li');
         },
         activeSlider : function() {
-
             return el.li().eq(getActiveNo());
         },
         nav : {
@@ -48,10 +48,52 @@ $(function() {
 
 
     putButtonMiddle();
+    function showDots() {
+        var m = '';
+        for ( var i = 0 ; i < el.li().length ; i ++ ) {
+            m += '<b no="'+i+'">·</b>';
+        }
+        el.slider().append('<nav class="dots">' + m + '</nav>');
+        var $dots = el.slider().find('.dots');
+        $dots.css( {
+            'position': 'absolute',
+            'z-index': 987654321,
+            'bottom' : 0,
+            'left' : 0,
+            'right' : 0,
+            'text-align' : 'center',
+            'font-size': '40px',
+            'color': attr.dotColor,
+            'line-height' : '20px',
+            'cursor': 'pointer'
+        });
+        $dots.find(':eq(0)').css({
+            'color': 'white'
+        });
+        $dots.find('b').click(function(){
+            var no = $(this).attr('no');
+            console.log(no);
+            animateNo(no);
+        });
+    }
+    showDots();
+
+
+
     setInterval(animate, attr.intervalPageChange);
 
     //console.log( el.li().length );
 
+    function animateNo(no) {
+        if ( attr.direction == 'right-to-left' ) {
+            attr.active = parseInt(no) - 1;
+        }
+        else {
+            attr.active = parseInt(no) + 1;
+        }
+        console.log('attr.active:' + attr.active);
+        animate(true);
+    }
     function animate( force ) {
 
         if ( ! force ) if ( attr.mouseIn ) return;
@@ -67,10 +109,13 @@ $(function() {
             left = -w;
         }
 
+        moveDots();
+
         var $act = el.activeSlider();// $li.eq(getActiveNo());//getActiveSlider();
+        var z = getNextIndex();
         $act.css({
             'display' : 'block'
-            , 'z-index': getNextIndex()
+            , 'z-index': z
             , 'left' : left
             , 'width' : w + 1
         } );
@@ -87,6 +132,14 @@ $(function() {
 
         putButtonMiddle();
     }
+
+
+    function moveDots() {
+        var $dots = el.slider().find('.dots');
+        $dots.find('b').css('color', attr.dotColor);
+        $dots.find('b:eq("'+getActiveNo()+'")').css('color', 'white');
+    }
+
 
     function increaseActiveNo() {
         ++ attr.active;
