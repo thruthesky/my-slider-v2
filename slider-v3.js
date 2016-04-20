6e79432b5f6ca85750207afe8b3ec43e7813ef95
thruthesky <thruthesky@gmail.com>
2016-04-20 22:39:34 +0900

    Reverting Mr. Song's code

--- slider-v3.js
+++ slider-v3.js
@@ -1,13 +1,14 @@
 (
     function( $ ) {
         $.fn.animateMySlider = function ( options ) {
+            //console.log(this.length);
             this.each ( function () {
                 var $this = $(this);
 
 
                 // variables
                 var settings = $.extend( {
-                    intervalPageChange : 800,      
+                    intervalPageChange : 800,
                     speedPageChange : 500,
                     direction : 'right-to-left',
                     active : 0,
@@ -32,29 +33,46 @@
                     },
                     nav : {
                         left : function () {
-                            return el.slider().find('nav img:eq(0)')     
+                            return el.slider().find('nav img:eq(0)');
                         },
                         right : function () {
                             return el.slider().find('nav img:eq(1)');
                         }
                     },
                     navs : function() {
-                        return el.slider().find('nav img');
+                        return el.slider().find('nav');
                     }
+
                 };
 
 
                 // listeners
+                //console.log($this);
                 el.slider()
                     .mouseenter(pauseAnimation)
                     .mouseleave(resumeAnimation);
 
-             
+
+                el.nav.left().click(function(){
+                    //console.log('left clicked');
+                    if ( inAnimation ) return; // console.log('but in animation. returns.');
+                    settings.direction = 'left-to-right';
+                    animate(true);
+                });
+                el.nav.right().click(function(){
+                    // console.log('right clicked');
+                    if ( inAnimation ) return; // console.log('but in animation. returns.');
+                    settings.direction = 'right-to-left';
+                    animate(true);
+                });
+
+
                 // initialize
                 showDots();
                 setTimeout(putNavButtonMiddle, 10);
+
                 setInterval(animate, settings.intervalPageChange);
-                navButtonClick();
+
 
 
                 // Animation
@@ -65,9 +83,11 @@
                     else {
                         settings.active = parseInt(no) + 1;
                     }
+                    // console.log('settings.active:' + settings.active);
                     animate(true);
-                    }   
+                }
                 function animate( force ) {
+
                     if ( ! force ) if ( settings.mouseIn ) return;
 
                     var w = el.slider().width();
@@ -83,7 +103,7 @@
 
                     moveDots();
 
-                    var $act = el.activeSlider();
+                    var $act = el.activeSlider();// $li.eq(getActiveNo());//getActiveSlider();
                     var z = getNextIndex();
                     $act.css({
                         'display' : 'block'
@@ -101,6 +121,7 @@
                     }
 
                     resizeHeight();
+
                     putNavButtonMiddle();
                 }
 
@@ -109,7 +130,7 @@
                 function showDots() {
                     var m = '';
                     for ( var i = 0 ; i < el.li().length ; i ++ ) {
-                        m += '<b no="'+i+'"><i class="fa fa-circle" aria-hidden="true"></i></b>'; 
+                        m += '<b no="'+i+'">-</b>';
                     }
                     el.slider().append('<nav class="dots">' + m + '</nav>');
                     var $dots = el.slider().find('.dots');
@@ -120,14 +141,11 @@
                         'left' : 0,
                         'right' : 0,
                         'text-align' : 'center',
-                        'font-size': '20px',    
-                        'color': settings.dotColor, 
+                        'font-size': '40px',
+                        'color': settings.dotColor,
                         'line-height' : '20px',
                         'cursor': 'pointer'
-                    });                  
-                            
-                    el.slider().find('.fa-circle').css('padding-left', '4px');
-                    
+                    });
                     $dots.find(':eq(0)').css({
                         'color': 'white'
                     });
@@ -167,25 +185,23 @@
 
 
                 function putNavButtonMiddle() {
+                    // var h_slider = el.activeSlider().height(); // This height is based on the height of the image ( LI tag ). So, the button moves based on the image size.
                     var h_slider = el.slider().height();
+
+                    //console.log('h_slier: ' + h_slider);
+
+
                     var h_button = el.nav.left().height();
+
+                    //console.log('h_button: ' + h_button);
+
                     var pos_top = parseInt( h_slider / 2 - h_button / 2 );
+
+
                     el.nav.left().css('top', pos_top);
                     el.nav.right().css('top', pos_top);
-                    el.navs().css('display', 'block');
-                }
 
-                 function navButtonClick() {
-                    if (el.nav.left().click(function(){
-                        if ( inAnimation ) return; 
-                    settings.direction = 'left-to-right';
-                    animate(true);
-                    }));
-                    if (el.nav.right().click(function(){
-                        if ( inAnimation ) return; 
-                        settings.direction = 'right-to-left';
-                        animate(true);
-                    }));       
+                    el.navs().css('opacity', '1');
                 }
 
                 function pauseAnimation() {
